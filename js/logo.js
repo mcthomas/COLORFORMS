414b3b5596d5315b77c844bd600a0ba32157fe7b
mcthomas <matt.thomas2@icloud.com>
2022-06-14 22:21:19 -0500

    Add logo pong basic input handling

--- js/logo.js
+++ js/logo.js
@@ -1,5 +1,8 @@
 var output = function (input) {
     var winSize = 220;
+    var gameMode = false;
+    ball = null;
+    activePaddle = null;
     //Divides a line into three proportional to the window size; q1 is a start coord, q2 is length, q3 is end coord
     var q1 = .25*winSize;
     var q2 = .5*winSize;
@@ -30,6 +33,9 @@ var output = function (input) {
         //Rouding right segment, top then bottom
         roundCorners(winSize-lineWidth, q1, winSize-lineWidth, q1+7, winSize-.25*lineWidth, q1+3, winSize-.75*lineWidth, q1+3, winSize, q1+7, winSize, q1, winSize-lineWidth, q1);
         roundCorners(winSize-lineWidth, q3, winSize-lineWidth, q3-7, winSize-.25*lineWidth, q3-3, winSize-.75*lineWidth, q3-3, winSize, q3-7, winSize, q3, winSize-lineWidth, q3);
+        if(gameMode){
+          game();
+        }
     }
     var color = function() {
       for(var i = 0; i < q2; i+=1) {
@@ -141,5 +147,171 @@ var output = function (input) {
       input.vertex(x7, y7);
       input.endShape();
     }
+    input.mousePressed = function() {
+      if(!gameMode && input.mouseX > 0 && input.mouseX < winSize && input.mouseY > 0 && input.mouseY < winSize) {
+        gameMode = true;
+        disableKeyScroll();
+      }
+    }
+    input.keyPressed = function() {
+
+    }
+    var game = function () {
+      drawBall();
+      collisionDetector();
+      paddleHandler();
+    }
+    var drawBall = function () {
+      if(ball == null) {
+        ball = new Ball();
+      }
+      input.noStroke();
+      input.fill(64,64,64);
+      input.circle(ball.x, ball.y, 1.25*lineWidth); 
+      ball.x += ball.dx;
+      ball.y += ball.dy;
+    }
+    class Ball {
+      constructor() {
+        this.x = winSize/2;
+        this.y = winSize/2;
+        this.dx = -0.5 + Math.random();
+        this.dy = -0.5 + Math.random();
+        //Here we do some extra math to ensure dx and dy will add to 1 (for consistent speed)
+        if (this.dy < 0) {
+          this.dy = -1 * (1 - input.abs(this.dx));
+        }
+        else if (this.dy > 0) {
+          this.dy = 1 - input.abs(this.dx);
+        }
+      }
+    }
+  var collisionDetector = function () {
+    //The four wall collisions; padding of 5 added for center-ball tracking radius
+    if((ball.x-5 <= lineWidth) && (ball.y+5 >= q1) && (ball.y-5 <= q3)) {
+      ball.dx = collisionResolver(ball.dx);
+    } 
+    if((ball.x+5 >= winSize - lineWidth) && (ball.y+5 >= q1) && (ball.y-5 <= q3)) {
+      ball.dx = collisionResolver(ball.dx);
+    } 
+    if((ball.x+5 >= q1) && (ball.x-5 <= q3) && (ball.y-5 <= lineWidth)) {
+      ball.dy = collisionResolver(ball.dy);
+    } 
+    if((ball.x+5 >= q1) && (ball.x-5 <= q3) && (ball.y+5 >= winSize - lineWidth)) {
+      ball.dy = collisionResolver(ball.dy);
+    } 
+    //The four paddle collisions
+    /*
+    if(activePaddle == "LU" && intersects(somex, somey)) {
+      reflect ball
+    }
+    if(activePaddle == "RU" && intersects(somex, somey)) {
+      reflect ball
+    }
+    if(activePaddle == "LD" && intersects(somex, somey)) {
+      reflect ball
+    }
+    if(activePaddle == "RD" && intersects(somex, somey)) {
+      reflect ball
+    }
+    */
+  }
+  var collisionResolver = function (axis) {
+    axis = -1 * axis;
+    return axis
+  }
+  var paddleHandler = function () {
+    //left 37, right 39, up 38, down 40
+    if (input.keyIsDown(37) && input.keyIsDown(38) && !input.keyIsDown(39) && !input.keyIsDown(40)) {
+      //LU
+      input.fill(175, 175, 175);
+      activePaddle = "LU";
+      drawPaddle("LU");
+    }
+    else if (input.keyIsDown(39) && input.keyIsDown(38) && !input.keyIsDown(37) && !input.keyIsDown(40)) {
+      //RU
+      input.fill(175, 175, 175);
+      activePaddle = "RU"
+      drawPaddle("RU");
+    }
+    else if (input.keyIsDown(37) && input.keyIsDown(40) && !input.keyIsDown(38) && !input.keyIsDown(39)) {
+      //LD
+      input.fill(175, 175, 175);
+      activePaddle = "LD"
+      drawPaddle("LD");
+    }
+    else if (input.keyIsDown(39) && input.keyIsDown(40) && !input.keyIsDown(37) && !input.keyIsDown(38)) {
+      //RD
+      input.fill(175, 175, 175);
+      activePaddle = "RD"
+      drawPaddle("RD");
+    }
+    else if (input.keyIsDown(37) && !input.keyIsDown(38) && !input.keyIsDown(39) && !input.keyIsDown(40)) {
+      //L
+      input.noFill();
+      input.stroke(175, 175, 175)
+      activePaddle = null;
+      drawPaddle("LU");
+      drawPaddle("LD");
+    }
+    else if (input.keyIsDown(39) && !input.keyIsDown(37) && !input.keyIsDown(38) && !input.keyIsDown(40)) {
+      //R
+      input.noFill();
+      input.stroke(175, 175, 175)
+      activePaddle = null;
+      drawPaddle("RU");
+      drawPaddle("RD");
+    }
+    else if (input.keyIsDown(38) && !input.keyIsDown(37) && !input.keyIsDown(39) && !input.keyIsDown(40)) {
+      //U
+      input.noFill();
+      input.stroke(175, 175, 175)
+      activePaddle = null;
+      drawPaddle("LU");
+      drawPaddle("RU");
+    }
+    else if (input.keyIsDown(40) && !input.keyIsDown(37) && !input.keyIsDown(38) && !input.keyIsDown(39)) {
+      //D
+      input.noFill();
+      input.stroke(175, 175, 175)
+      activePaddle = null;
+      drawPaddle("LD");
+      drawPaddle("RD");
+    }
+  }
+  var drawPaddle = function (paddle) {
+    input.push();
+    input.angleMode(input.DEGREES);
+    switch (paddle) {
+      case "LU":
+        input.translate(lineWidth-10, q1-2.5);
+        input.rotate(315);
+        input.rect(0, 0, q1*1.37, 10, 5);
+        break;
+      case "RU":
+        input.translate(q3+2.5, 0);
+        input.rotate(45);
+        input.rect(0, 0, q1*1.37, 10, 5);
+        break;
+      case "LD":
+        input.translate(lineWidth-2.5, q3-5);
+        input.rotate(45);
+        input.rect(0, 0, q1*1.37, 10, 5);
+        break;
+      case "RD":
+        input.translate(q3-4.5, q3+q1-6.5);
+        input.rotate(315);
+        input.rect(0, 0, q1*1.37, 10, 5);
+        break
+    }
+    input.pop();
+  }
+  var disableKeyScroll = function () {
+    window.addEventListener("keydown", function(e) {
+        if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+            e.preventDefault();
+        }
+    }, false);
+  }
 }
 var logoDisplay = new p5(output, "logo");
